cc989aad5fb8f02201acc145abd2516f20ed7332
Chino Chang <chino@joymoe.com>
2020-06-25 00:38:14 +0800

    Add Int64StringConverter

---
+++ src/JoyMoe.Common.Json/Int64StringConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JoyMoe.Common.Json
+{
+    /// <summary>
+    /// String JsonConverter for Int64
+    /// </summary>
+    public class Int64StringConverter : JsonConverter<long>
+    {
+        public override long Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var span = reader.HasValueSequence ? reader.ValueSequence.ToArray() : reader.ValueSpan;
+                if (Utf8Parser.TryParse(span, out long number, out int bytesConsumed) && span.Length == bytesConsumed)
+                    return number;
+
+                if (long.TryParse(reader.GetString(), out number))
+                    return number;
+            }
+
+            return reader.GetInt64();
+        }
+
+        public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
