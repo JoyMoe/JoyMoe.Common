e6837a89c5dbf1710643603ae7f3665f21db1a61
Chino Chang <chino@joymoe.com>
2020-06-25 00:38:14 +0800

    Add Int64StringConverter

--- src/JoyMoe.Common.Json/JsonOptionsExtensions.cs
+++ src/JoyMoe.Common.Json/JsonOptionsExtensions.cs
@@ -12,12 +12,29 @@ namespace JoyMoe.Common.Json
         /// <returns></returns>
         public static JsonOptions UseSnakeCaseNamingPolicy(this JsonOptions option)
         {
-            if (option == null)
+            if (option?.JsonSerializerOptions == null)
             {
                 throw new ArgumentNullException(nameof(option));
             }
 
             option.JsonSerializerOptions.PropertyNamingPolicy = new SnakeCaseNamingPolicy();
+
+            return option;
+        }
+
+        /// <summary>
+        /// Configure <see cref="JsonOptions"/> to use <see cref="Int64StringConverter"/>
+        /// </summary>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public static JsonOptions UseInt64StringConverter(this JsonOptions option)
+        {
+            if (option?.JsonSerializerOptions == null)
+            {
+                throw new ArgumentNullException(nameof(option));
+            }
+
+            option.JsonSerializerOptions.Converters.Add(new Int64StringConverter());
 
             return option;
         }
@@ -29,7 +46,7 @@ namespace JoyMoe.Common.Json
         /// <returns></returns>
         public static JsonOptions UseIsoDateTimeConverter(this JsonOptions option)
         {
-            if (option == null)
+            if (option?.JsonSerializerOptions == null)
             {
                 throw new ArgumentNullException(nameof(option));
             }
