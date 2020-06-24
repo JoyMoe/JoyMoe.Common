04f24a39a11de09f3d7d04603f6dd2b02c9bf48e
Chino Chang <chino@joymoe.com>
2020-06-25 00:39:10 +0800

    cleanup

--- src/JoyMoe.Common.Session/CachedTicketStoreServiceCollectionExtensions.cs
+++ src/JoyMoe.Common.Session/CachedTicketStoreServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
