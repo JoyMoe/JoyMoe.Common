af5b18b1c04c9cb31532e4257c2ad51ba9e9a4a0
Chino Chang <chino@joymoe.com>
2020-06-26 13:19:28 +0800

    Add JoyMoe.Common.EntityFrameworkCore

---
+++ src/JoyMoe.Common.EntityFrameworkCore/DbContextBase.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JoyMoe.Common.EntityFrameworkCore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoyMoe.Common.EntityFrameworkCore
+{
+    public class DbContextBase : DbContext
+    {
+        public DbContextBase(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            AddTimestampsAsync().GetAwaiter().GetResult();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await AddTimestampsAsync();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task AddTimestampsAsync()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => x.Entity is IDataEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entity in entities)
+            {
+                if (!(entity.Entity is IDataEntity data)) continue;
+
+                var now = DateTimeOffset.Now;
+
+                if (entity.State == EntityState.Added)
+                {
+                    await OnCreateEntity(data);
+                    data.CreatedAt = now;
+                }
+
+                await OnUpdateEntity(data);
+                data.UpdatedAt = now;
+            }
+        }
+
+        protected virtual Task OnCreateEntity(IDataEntity entity)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task OnUpdateEntity(IDataEntity entity)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
