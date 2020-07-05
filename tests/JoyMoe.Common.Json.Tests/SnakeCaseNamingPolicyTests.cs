4a9dc0a5809707d7ce83b522133d0ee41a3bba2a
Chino Chang <chino@joymoe.com>
2020-07-05 22:39:11 +0800

    Add JoyMoe.Common.Json.Tests

---
+++ tests/JoyMoe.Common.Json.Tests/SnakeCaseNamingPolicyTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace JoyMoe.Common.Json.Tests
+{
+    public class SnakeCaseNamingPolicyTests
+    {
+        [Fact]
+        public void ConvertNameTest()
+        {
+            var policy = new SnakeCaseNamingPolicy();
+
+            Assert.Equal("foo", policy.ConvertName("Foo"));
+            Assert.Equal("foo_bar", policy.ConvertName("FooBar"));
+        }
+    }
+}
