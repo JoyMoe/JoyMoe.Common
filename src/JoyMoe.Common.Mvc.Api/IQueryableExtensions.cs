83c1581a6d4816c4aa206a3feca955a5bf09ef34
7IN0SAN9 <me@7in0.me>
2020-07-11 20:55:03 +0800

    Use GetAwaiter().GetResult()

--- src/JoyMoe.Common.Mvc.Api/IQueryableExtensions.cs
+++ src/JoyMoe.Common.Mvc.Api/IQueryableExtensions.cs
@@ -8,25 +8,6 @@ namespace System.Linq
 {
     public static class PaginationResponseIQueryableExtensions
     {
-        private static IQueryable<T> prepare<T, TEntity>(
-            this IQueryable<TEntity> query,
-            PaginationRequest request,
-            Expression<Func<TEntity, int, T>> expression)
-            where T : IIdentifier
-            where TEntity : IIdentifier
-        {
-            if (request.Before.HasValue)
-            {
-                query = query.Where(a => a.Id < request.Before);
-            }
-
-            query = query.OrderByDescending(a => a.Id);
-
-            return query
-                .Take(request.Size)
-                .Select(expression);
-        }
-
         public async static Task<PaginationResponse<T>> ToPaginationResponseAsync<T, TEntity>(
             this IQueryable<TEntity> query,
             PaginationRequest request,
@@ -39,7 +20,14 @@ namespace System.Linq
                 throw new ArgumentNullException(nameof(request));
             }
 
-            var data = await prepare(query, request, expression)
+            if (request.Before.HasValue)
+            {
+                query = query.Where(a => a.Id < request.Before);
+            }
+
+            var data = await query.OrderByDescending(a => a.Id)
+                .Take(request.Size)
+                .Select(expression)
                 .ToListAsync()
                 .ConfigureAwait(false);
 
@@ -53,15 +41,10 @@ namespace System.Linq
             where T : IIdentifier
             where TEntity : IIdentifier
         {
-            if (request == null)
-            {
-                throw new ArgumentNullException(nameof(request));
-            }
-
-            var data = prepare(query, request, expression)
-                .ToList();
-
-            return new PaginationResponse<T>(data, request.Before);
+            return query.ToPaginationResponseAsync(request, expression)
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
         }
     }
 }
