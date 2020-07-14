92250947f98cfa8e8419ece2dbcecf810b024b64
Chino Chang <chino@joymoe.com>
2020-07-14 16:52:06 +0800

    re-target Storage.* to netstandard2.0

---
+++ src/JoyMoe.Common.Storage/SystemNetHttpExtensions.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+// ReSharper disable once CheckNamespace
+namespace System.Net.Http
+{
+    public static class HttpHeadersExtensions
+    {
+        private static string AppendParameter(string query, KeyValuePair<string, string> pair)
+        {
+            var kv = string.IsNullOrWhiteSpace(pair.Value)
+                    ? $"{Uri.EscapeDataString(pair.Key)}"
+                    : $"{Uri.EscapeDataString(pair.Key)}={Uri.EscapeDataString(pair.Value)}";
+
+            if (query.Length == 0)
+            {
+                query = kv;
+            }
+            else
+            {
+                query += $"&{kv}";
+            }
+
+            return query;
+        }
+
+        public static Uri AddQueryParameter(this Uri uri, string key, string value)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            var url = uri.GetLeftPart(UriPartial.Path);
+            var query = uri.Query.TrimStart('?');
+
+            query = AppendParameter(query, new KeyValuePair<string, string>(key, value));
+
+            return new Uri($"{url}?{query}", uri.IsAbsoluteUri ? UriKind.Absolute : UriKind.Relative);
+        }
+
+        public static Uri AddQueryParameters(this Uri uri, IEnumerable<KeyValuePair<string, string>> pairs)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            if (pairs == null)
+            {
+                throw new ArgumentNullException(nameof(pairs));
+            }
+
+            var url = uri.GetLeftPart(UriPartial.Path);
+            var query = uri.Query.TrimStart('?');
+
+            query = pairs.Aggregate(query, AppendParameter);
+
+            return new Uri($"{url}?{query}", uri.IsAbsoluteUri ? UriKind.Absolute : UriKind.Relative);
+        }
+
+        public static string? FindFirstValue(this HttpHeaders headers, string key)
+        {
+            return headers.FirstOrDefault(h => h.Key == key).Value?.FirstOrDefault();
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> ToQueryKeyValuePairs(this Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            return uri.Query.TrimStart('?').Split('&')
+                .Where(s => s.Trim().Length > 0)
+                .Select(s => s.Split('='))
+                .Select(sp => sp.Length == 2
+                    ? new KeyValuePair<string, string>(Uri.UnescapeDataString(sp[0]), Uri.UnescapeDataString(sp[1]))
+                    : new KeyValuePair<string, string>(Uri.UnescapeDataString(sp[0]), ""));
+        }
+    }
+}
