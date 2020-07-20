25ead88ef45500e84272d4fbf76d51663756250b
Chino Chang <chino@joymoe.com>
2020-07-20 18:09:06 +0800

    Run Select locally to avoid dotnet/efcore#21583

--- src/JoyMoe.Common.Mvc.Api/IQueryableExtensions.cs
+++ src/JoyMoe.Common.Mvc.Api/IQueryableExtensions.cs
@@ -10,12 +10,17 @@ namespace System.Linq
     public static class PaginationResponseIQueryableExtensions
     {
         public async static Task<PaginationResponse<T>> ToPaginationResponseAsync<T, TEntity>(
-            this IQueryable<TEntity> query,
+            this IQueryable<IDataEntity> query,
             PaginationRequest request,
-            Expression<Func<TEntity, int, T>> expression)
+            Func<TEntity, int, T> expression)
             where T : IIdentifier
             where TEntity : IIdentifier
         {
+            if (!(query is IQueryable<TEntity>))
+            {
+                throw new NotSupportedException();
+            }
+
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
@@ -26,23 +31,24 @@ namespace System.Linq
                 query = query.Where(a => a.Id < request.Before);
             }
 
-            var data = await query.OrderByDescending(a => a.Id)
+            var entities = await query.OrderByDescending(a => a.Id)
                 .Take(request.Size)
-                .Select(expression)
                 .ToListAsync()
                 .ConfigureAwait(false);
+
+            var data = entities.Cast<TEntity>().Select(expression).ToList();
 
             return new PaginationResponse<T>(data, request.Before);
         }
 
         public static PaginationResponse<T> ToPaginationResponse<T, TEntity>(
-            this IQueryable<TEntity> query,
+            this IQueryable<IDataEntity> query,
             PaginationRequest request,
-            Expression<Func<TEntity, int, T>> expression)
+            Func<TEntity, int, T> expression)
             where T : IIdentifier
             where TEntity : IIdentifier
         {
-            return query.ToPaginationResponseAsync(request, expression)
+            return query.ToPaginationResponseAsync<T, TEntity>(request, expression)
                 .ConfigureAwait(false)
                 .GetAwaiter()
                 .GetResult();
