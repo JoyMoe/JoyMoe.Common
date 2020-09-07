e06e7881660b3a0e177f15b50f3228b6a7a9c730
Chino Chang <chino@joymoe.com>
2020-09-07 23:49:47 +0800

    Remove IIdentifier

--- src/JoyMoe.Common.EntityFrameworkCore/Models/IDataEntity.cs
+++ src/JoyMoe.Common.EntityFrameworkCore/Models/IDataEntity.cs
@@ -4,6 +4,8 @@ namespace JoyMoe.Common.EntityFrameworkCore.Models
 {
     public interface IDataEntity
     {
+        long Id { get; set; }
+
         DateTimeOffset CreatedAt { get; set; }
 
         DateTimeOffset UpdatedAt { get; set; }
