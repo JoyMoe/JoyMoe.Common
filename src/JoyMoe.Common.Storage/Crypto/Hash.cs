7945a446ffa0a18646dbd5d5353bb40e1b6203f1
Chino Chang <chino@joymoe.com>
2020-09-09 15:45:18 +0800

    Remove JoyMoe.Common.Crypto

--- src/JoyMoe.Common.Storage/Crypto/Hash.cs
+++ src/JoyMoe.Common.Storage/Crypto/Hash.cs
@@ -3,67 +3,70 @@ using System.Security.Cryptography;
 using System.Text;
 
 // ReSharper disable once CheckNamespace
-namespace System
+namespace JoyMoe.Common.Storage
 {
     public static class Hash
     {
         public static byte[] Md5(this string cipher)
         {
-            return Encoding.UTF8.GetBytes(cipher).Md5();
+            var bytes = Encoding.UTF8.GetBytes(cipher);
+            return bytes.Md5();
         }
 
-        public static byte[] Md5(this byte[] cipher)
+        public static byte[] Md5(this byte[] bytes)
         {
 #pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
             using var md5 = MD5.Create();
 #pragma warning restore CA5351 // Do Not Use Broken Cryptographic Algorithms
-            return md5.ComputeHash(cipher);
+            return md5.ComputeHash(bytes);
         }
 
-        public static byte[] Md5(this Stream cipher)
+        public static byte[] Md5(this Stream stream)
         {
 #pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
             using var md5 = MD5.Create();
 #pragma warning restore CA5351 // Do Not Use Broken Cryptographic Algorithms
-            return md5.ComputeHash(cipher);
+            return md5.ComputeHash(stream);
         }
 
         public static byte[] Sha1(this string cipher)
         {
-            return Encoding.UTF8.GetBytes(cipher).Sha1();
+            var bytes = Encoding.UTF8.GetBytes(cipher);
+            return bytes.Sha1();
         }
 
-        public static byte[] Sha1(this byte[] cipher)
+        public static byte[] Sha1(this byte[] bytes)
         {
 #pragma warning disable CA5350 // Do Not Use Weak Cryptographic Algorithms
             using var sha1 = SHA1.Create();
 #pragma warning restore CA5350 // Do Not Use Weak Cryptographic Algorithms
-            return sha1.ComputeHash(cipher);
+            return sha1.ComputeHash(bytes);
         }
 
-        public static byte[] Sha1(this Stream cipher)
+        public static byte[] Sha1(this Stream stream)
         {
 #pragma warning disable CA5350 // Do Not Use Weak Cryptographic Algorithms
             using var sha1 = SHA1.Create();
 #pragma warning restore CA5350 // Do Not Use Weak Cryptographic Algorithms
-            return sha1.ComputeHash(cipher);
+            return sha1.ComputeHash(stream);
         }
 
         public static byte[] Sha256(this string cipher)
         {
-            return Encoding.UTF8.GetBytes(cipher).Sha256();
+            var bytes = Encoding.UTF8.GetBytes(cipher);
+            return bytes.Sha256();
         }
 
-        public static byte[] Sha256(this byte[] cipher)
+        public static byte[] Sha256(this byte[] bytes)
         {
             using var sha256 = SHA256.Create();
-            return sha256.ComputeHash(cipher);
+            return sha256.ComputeHash(bytes);
         }
 
-        public static byte[] Sha256(this Stream cipher)
+        public static byte[] Sha256(this Stream stream)
         {
             using var sha256 = SHA256.Create();
-            return sha256.ComputeHash(cipher);
+            return sha256.ComputeHash(stream);
         }
     }
 }
