b1ce20faefcb94eba583f50296a46651529bd646
Chino Chang <chino@joymoe.com>
2020-09-09 15:45:18 +0800

    Remove JoyMoe.Common.Crypto

---
+++ src/JoyMoe.Common.Storage/Crypto/Helper.cs
@@ -0,0 +1,17 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace JoyMoe.Common.Storage
+{
+    public static class Helper
+    {
+        public static string ToHex(this byte[] bytes)
+        {
+#pragma warning disable CA1308 // Normalize strings to uppercase
+            return BitConverter.ToString(bytes)
+                .ToLowerInvariant()
+                .Replace("-", string.Empty);
+#pragma warning restore CA1308 // Normalize strings to uppercase
+        }
+    }
+}
