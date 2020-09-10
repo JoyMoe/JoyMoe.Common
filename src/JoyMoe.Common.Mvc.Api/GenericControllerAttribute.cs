e00352efe814388b351e460147ebc63d124cb048
Chino Chang <chino@joymoe.com>
2020-09-10 15:25:00 +0800

    Pluralize the model name

--- src/JoyMoe.Common.Mvc.Api/GenericControllerAttribute.cs
+++ src/JoyMoe.Common.Mvc.Api/GenericControllerAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 namespace JoyMoe.Common.Mvc.Api
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class GenericControllerAttribute : Attribute, IControllerModelConvention
     {
         public void Apply(ControllerModel controller)
@@ -13,9 +13,15 @@ namespace JoyMoe.Common.Mvc.Api
                 throw new ArgumentNullException(nameof(controller));
             }
 
+            if (!controller.ControllerType.IsGenericType ||
+                controller.ControllerType.GetGenericTypeDefinition() != typeof(GenericController<,,>))
+            {
+                return;
+            }
+
             var entityType = controller.ControllerType.GetGenericArguments()[0];
 
-            controller.ControllerName = entityType.Name;
+            controller.ControllerName = entityType.Name.Pluralize();
         }
     }
 }
---
+++ src/JoyMoe.Common.Mvc.Api/Pluralizer.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+
+namespace JoyMoe.Common.Mvc.Api
+{
+    public static class Pluralizer
+    {
+        public static string Pluralize(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            var exceptions = new Dictionary<string, string>()
+            {
+                {"man", "men"},
+                {"woman", "women"},
+                {"child", "children"},
+                {"tooth", "teeth"},
+                {"foot", "feet"},
+                {"mouse", "mice"},
+                {"belief", "beliefs"}
+            };
+
+#pragma warning disable CA1308 // Normalize strings to uppercase
+            if (exceptions.ContainsKey(text.ToLowerInvariant()))
+            {
+                return exceptions[text.ToLowerInvariant()];
+            }
+#pragma warning restore CA1308 // Normalize strings to uppercase
+
+            if (text.EndsWith("y", StringComparison.OrdinalIgnoreCase) &&
+                !text.EndsWith("ay", StringComparison.OrdinalIgnoreCase) &&
+                !text.EndsWith("ey", StringComparison.OrdinalIgnoreCase) &&
+                !text.EndsWith("iy", StringComparison.OrdinalIgnoreCase) &&
+                !text.EndsWith("oy", StringComparison.OrdinalIgnoreCase) &&
+                !text.EndsWith("uy", StringComparison.OrdinalIgnoreCase))
+            {
+                return text.Substring(0, text.Length - 1) + "ies";
+            }
+
+            if (text.EndsWith("us", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // http://en.wikipedia.org/wiki/Plural_form_of_words_ending_in_-us
+                return text + "es";
+            }
+
+            if (text.EndsWith("ss", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return text + "es";
+            }
+
+            if (text.EndsWith("s", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return text;
+            }
+
+            if (text.EndsWith("x", StringComparison.InvariantCultureIgnoreCase) ||
+                text.EndsWith("ch", StringComparison.InvariantCultureIgnoreCase) ||
+                text.EndsWith("sh", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return text + "es";
+            }
+
+            if (text.EndsWith("f", StringComparison.InvariantCultureIgnoreCase) && text.Length > 1)
+            {
+                return text.Substring(0, text.Length - 1) + "ves";
+            }
+
+            if (text.EndsWith("fe", StringComparison.InvariantCultureIgnoreCase) && text.Length > 2)
+            {
+                return text.Substring(0, text.Length - 2) + "ves";
+            }
+
+            return text + "s";
+        }
+    }
+}
