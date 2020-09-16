1fa496aacf9cdb393e33d44c5e6672b76a2deac7
Chino Chang <chino@joymoe.com>
2020-09-16 19:53:46 +0800

    add back IIdentifier

--- src/JoyMoe.Common.EntityFrameworkCore/Models/IDataEntity.cs
+++ src/JoyMoe.Common.EntityFrameworkCore/Models/IDataEntity.cs
@@ -2,10 +2,8 @@ using System;
 
 namespace JoyMoe.Common.EntityFrameworkCore.Models
 {
-    public interface IDataEntity
+    public interface IDataEntity : IIdentifier
     {
-        long Id { get; set; }
-
         DateTimeOffset CreatedAt { get; set; }
 
         DateTimeOffset UpdatedAt { get; set; }
