9491cb0dcd7c209afe12b28a62d9193726124a3b
Chino Chang <chino@joymoe.com>
2021-03-17 16:17:21 +0800

    mark DataAnnotations on interfaces

---
+++ src/JoyMoe.Common.Data/IConcurrency.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JoyMoe.Common.Data
+{
+    public interface IConcurrency
+    {
+        [Timestamp]
+        Guid Timestamp { get; set; }
+    }
+}
--- src/JoyMoe.Common.Data/IIdentifier.cs
+++ src/JoyMoe.Common.Data/IIdentifier.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JoyMoe.Common.Data
 {
     public interface IIdentifier
     {
+        [Key]
         long Id { get; set; }
     }
 }
