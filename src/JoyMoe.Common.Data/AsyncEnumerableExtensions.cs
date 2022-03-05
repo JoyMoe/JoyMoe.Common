54577232e3d8bbca5408ec2c707c68dadf9af8b0
Chino Chang <chino@joymoe.com>
2022-03-05 22:22:57 +0800

    Add ToDictionaryAsync

--- src/JoyMoe.Common.Data/AsyncEnumerableExtensions.cs
+++ src/JoyMoe.Common.Data/AsyncEnumerableExtensions.cs
@@ -43,4 +43,56 @@ public static class AsyncEnumerableExtensions
 
         return result;
     }
+
+    public static Task<Dictionary<TKey, TSource>> ToDictionaryAsync<TSource, TKey>(
+        this IAsyncEnumerable<TSource>? source,
+        Expression<Func<TSource, TKey>> keySelector,
+        CancellationToken               ct = default)
+    {
+        return source.ToDictionaryAsync(keySelector, null, ct);
+    }
+
+    public static Task<Dictionary<TKey, TSource>> ToDictionaryAsync<TSource, TKey>(
+        this IAsyncEnumerable<TSource>? source,
+        Expression<Func<TSource, TKey>> keySelector,
+        IEqualityComparer<TKey>?        comparer,
+        CancellationToken               ct = default)
+    {
+        return source.ToDictionaryAsync(keySelector, e => e, comparer, ct);
+    }
+
+
+    public static Task<Dictionary<TKey, TElement>> ToDictionaryAsync<TSource, TKey, TElement>(
+        this IAsyncEnumerable<TSource>?     source,
+        Expression<Func<TSource, TKey>>     keySelector,
+        Expression<Func<TSource, TElement>> elementSelector,
+        CancellationToken                   ct = default)
+    {
+        return source.ToDictionaryAsync(keySelector, elementSelector, null, ct);
+    }
+
+    public static async Task<Dictionary<TKey, TElement>> ToDictionaryAsync<TSource, TKey, TElement>(
+        this IAsyncEnumerable<TSource>?     source,
+        Expression<Func<TSource, TKey>>     keySelector,
+        Expression<Func<TSource, TElement>> elementSelector,
+        IEqualityComparer<TKey>?            comparer,
+        CancellationToken                   ct = default)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        var keyConverter     = keySelector.Compile();
+        var elementConverter = elementSelector.Compile();
+
+        var result = new Dictionary<TKey, TElement>(comparer);
+
+        await foreach (var item in source.WithCancellation(ct))
+        {
+            result.TryAdd(keyConverter(item), elementConverter(item));
+        }
+
+        return result;
+    }
 }
